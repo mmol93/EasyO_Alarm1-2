b133fa1e0ee1c192a1a3766f169cfb69c123e0bf
ljws93 <ljws93@naver.com>
2021-02-07 14:09:00 +0900

    created AlarmSet Activity

--- app/src/main/java/com/example/easyo_alarm/AlarmSetActivity.kt
+++ app/src/main/java/com/example/easyo_alarm/AlarmSetActivity.kt
@@ -19,6 +19,31 @@ class AlarmSetActivity : AppCompatActivity() {
         val adRequest = AdRequest.Builder().build()
         binder.adView.loadAd(adRequest)
 
+        // numberPicker의 시간 부분 최대, 최소값 설정
+        binder.numberPickerHour.maxValue = 12
+        binder.numberPickerHour.minValue = 1
+
+        // numberPicker의 분 부분 최대, 최소값 설정
+        binder.numberPickerMin.maxValue = 59
+        binder.numberPickerMin.minValue = 0
+
+        // numberPicker에 오전 오후 텍스트 세팅
+        val arg1 = arrayOf<String>(getString(R.string.alarmSet_AM), getString(R.string.alarmSet_PM))
+        binder.numberPickerAMPM.minValue = 0
+        binder.numberPickerAMPM.maxValue = arg1.size - 1
+        binder.numberPickerAMPM.displayedValues = arg1
+
+        // Cancel 버튼 클릭 시
+        binder.buttonCancel.setOnClickListener {
+            finish()
+        }
+
+        // Save 버튼 클릭 시
+        binder.buttonSave.setOnClickListener {
+
+        }
+
+
         setContentView(binder.root)
     }
 }
