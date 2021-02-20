0b0b972e147783e3823da71688ef48ce44a7300e
ljws93 <ljws93@naver.com>
2021-02-20 13:26:36 +0900

    setUp each imageView for setting RecyclerView

--- app/src/main/java/com/example/easyo_alarm/SettingRecyclerAdapter.kt
+++ app/src/main/java/com/example/easyo_alarm/SettingRecyclerAdapter.kt
@@ -18,13 +18,22 @@ class SettingRecyclerAdapter(val context : Context) : RecyclerView.Adapter<Setti
     }
 
     override fun onBindViewHolder(holder: SettingViewHolder, position: Int) {
+        // *** RecyclerView의 각 item 설정하기
+        // ** item 중 텍스트뷰 설정하기
         val item1 = context.getString(R.string.settingItem_selectAlarm)
         val item2 = context.getString(R.string.settingItem_alarmMode)
         val item3 = context.getString(R.string.settingItem_notification)
         val item4 = context.getString(R.string.settingItem_info)
         val item = arrayOf(item1, item2, item3, item4)
         holder.row_mainText.text = item[position]
-        Log.d("settingRecyclerAdapter", "item: ${item[position]}")
+
+        // ** item 중 이미지뷰 설정하기
+        when(position){
+            0 -> {holder.row_image.setImageResource(R.drawable.setting_select_alarm)}
+            1 -> {holder.row_image.setImageResource(R.drawable.calculator)}
+            2 -> {holder.row_image.setImageResource(R.drawable.setting_notification)}
+            3 -> {holder.row_image.setImageResource(R.drawable.setting_info)}
+        }
     }
 }
 
@@ -32,4 +41,5 @@ class SettingViewHolder(view : View) : RecyclerView.ViewHolder(view){
     val binder : SettingRowBinding = SettingRowBinding.bind(view)
 
     val row_mainText = binder.settingText
+    val row_image = binder.settingImage
 }
