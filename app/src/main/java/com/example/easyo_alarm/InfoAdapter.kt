5dcfc88b3a17b798bb095468eb5d6bfe4ddb5c21
Ljw <ljws93@naver.com>
2021-03-04 13:16:57 +0900

    setUp openSource ListAlertDialog & contents for that

--- app/src/main/java/com/example/easyo_alarm/InfoAdapter.kt
+++ app/src/main/java/com/example/easyo_alarm/InfoAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.easyo_alarm
 
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.example.easyo_alarm.databinding.InfoRowBinding
 
@@ -18,20 +22,67 @@ class InfoAdapter(val context : Context) : RecyclerView.Adapter<InfoViewHolder>(
 
     override fun onBindViewHolder(holder: InfoViewHolder, position: Int) {
         when(position){
-            // 0 : 후원탭
+            // * 0 : 후원탭
             0 ->{
                 holder.binder.infoImage.setImageResource(R.drawable.info_support)
-                holder.binder.infoText.context.getString(R.string.infoItem_support)
+                holder.binder.infoText.text = context.getString(R.string.infoItem_support)
+                holder.binder.infoSubText.text = context.getString(R.string.infoItem_subSupport)
+                holder.binder.rowItemView.setOnClickListener {
+                }
             }
-            // 1 : 문의탭
+            // * 1 : 문의탭
             1 -> {
                 holder.binder.infoImage.setImageResource(R.drawable.info_emal)
-                holder.binder.infoText.context.getString(R.string.infoItem_contact)
+                holder.binder.infoText.text = context.getString(R.string.infoItem_contact)
+                holder.binder.infoSubText.text = context.getString(R.string.infoItem_subContact)
+                holder.binder.rowItemView.setOnClickListener {
+
+                }
             }
-            // 2 : 오픈소스탭
+            // * 2 : 오픈소스탭
             2 ->{
                 holder.binder.infoImage.setImageResource(R.drawable.info_open)
-                holder.binder.infoText.context.getString(R.string.infoItem_version)
+                holder.binder.infoText.text = context.getString(R.string.infoItem_openSource)
+                holder.binder.infoSubText.text = context.getString(R.string.infoItem_subOpenSource)
+                holder.binder.infoSubText.text = ""
+                holder.binder.rowItemView.setOnClickListener {
+                    // 오픈소스 항목 입력
+                    val listItem = arrayOf("Apache2.0", "GNU General Public License v3.0", "com.github.iammert:ReadableBottomBar:0.2",
+                            "com.github.imtuann:FloatingActionButtonExpandable:1.1.2")
+                    // 리스트 다이얼로그 만들기
+                    val dialogBuilder = AlertDialog.Builder(context)
+                    dialogBuilder.setTitle(context.getString(R.string.infoItem_openSource))
+                    dialogBuilder.setNeutralButton(context.getString(R.string.cancelBtn), null)
+
+                    // 각 항목 클릭에 따른 리스너 설정
+                    val openSourceListener = object : DialogInterface.OnClickListener{
+                        override fun onClick(dialog: DialogInterface?, which: Int) {
+                            val intent = Intent(context, LicenseActivity::class.java)
+                            when(which){
+                                // Apache
+                                0 -> {
+                                    intent.putExtra("content", context.getString(R.string.license_Apache))
+                                }
+                                // GPL3.0
+                                1 -> {
+                                    intent.putExtra("content", context.getString(R.string.license_GPL))
+                                }
+                                // bottomTap
+                                2 ->{
+                                    intent.putExtra("content", context.getString(R.string.license_bottomTap))
+                                }
+                                // floatingButton
+                                3 -> {
+                                    intent.putExtra("content", context.getString(R.string.license_floatingButton))
+                                }
+                            }
+                            context.startActivity(intent)
+                        }
+                    }
+                    dialogBuilder.setItems(listItem, openSourceListener)
+                    dialogBuilder.show()
+
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/easyo_alarm/LicenseActivity.kt
@@ -0,0 +1,25 @@
+package com.example.easyo_alarm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.easyo_alarm.databinding.ActivityInfoBinding
+import com.example.easyo_alarm.databinding.ActivityLicenseBinding
+
+class LicenseActivity : AppCompatActivity() {
+    lateinit var binder : ActivityLicenseBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_license)
+
+        binder = ActivityLicenseBinding.inflate(layoutInflater)
+        setContentView(binder.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val textData = intent.getStringExtra("content")
+        binder.textView8.text = textData
+    }
+}
